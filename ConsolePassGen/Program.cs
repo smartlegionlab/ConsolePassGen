225eef97fc9f29acf5b8753e5889b50827a80961
A.A Suvorov <smartlegiondev@gmail.com>
2024-05-16 17:05:39 +0700

    Console Password Generator for Windows. v0.1.0

---
+++ ConsolePassGen/Program.cs
@@ -0,0 +1,86 @@
+﻿// Console Password Generator
+using System;
+
+class Strings
+{
+    public static readonly string UppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static readonly string LowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+    public static readonly string Digits = "0123456789";
+    public static readonly string SpecialCharacters = "!@#$%^&*()";
+}
+
+class BaseRandomGen
+{
+    private Random random = new Random();
+
+    public string GenerateRandomString(int length)
+    {
+        string allCharacters = Strings.UppercaseLetters + Strings.LowercaseLetters + Strings.Digits + Strings.SpecialCharacters;
+        char[] password = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            int randomIndex = random.Next(0, allCharacters.Length);
+            password[i] = allCharacters[randomIndex];
+        }
+
+        return new string(password);
+    }
+}
+
+class CharacterPrinter
+{
+    public void PrintCharacter(char symbol, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(symbol);
+        }
+        Console.WriteLine();
+    }
+}
+
+class ConsolePasswordGeneratorApp
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("*** Console Password Generator ***");
+        BaseRandomGen baseRandomGen = new BaseRandomGen();
+        string password = "";
+        Console.WriteLine("Введите длину пароля: ");
+        CharacterPrinter printer = new CharacterPrinter();
+        printer.PrintCharacter('-', 23);
+        int passwordLength = 0;
+        try
+        {
+            passwordLength = Convert.ToInt32(Console.ReadLine());
+            printer.PrintCharacter('-', 23);
+        }
+        catch (Exception e)
+        {
+            printer.PrintCharacter('-', 23);
+            passwordLength = 12;
+            Console.WriteLine("Ошибка! Вы не ввели длину пароля. Использую 12 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        if (passwordLength <= 0)
+        {
+            passwordLength = 12;
+            Console.WriteLine("Ошибка! Неверная длина пароля. Использую 12 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        else if (passwordLength > 1000)
+        {
+            passwordLength = 1000;
+            Console.WriteLine("Ошибка! Пароль не может быть длинее 1000 символов. Использую 1000 символов.");
+            printer.PrintCharacter('-', 23);
+        }
+        password = baseRandomGen.GenerateRandomString(passwordLength);
+        Console.WriteLine(password);
+        printer.PrintCharacter('-', 23);
+        Console.WriteLine("Нажмите <Enter> для выхода...");
+        Console.Read();
+        printer.PrintCharacter('-', 23);
+        Console.WriteLine("=== Smart Legion Lab ===");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ConsolePassGen
-Console Password Generator for Windows.
+
+***
+
+> Console password generator for Windows.
+
+***
+
+Author and developer: ___A.A Suvorov___
+
+***
+
+## Images:
+
+![logo](https://github.com/smartlegionlab/ConsolePassGen/raw/master/data/images/ConsolePassGen.png)
+
+***
+
+## Requirements:
+
+[.NET 8.0](https://aka.ms/dotnet-core-applaunch?missing_runtime=true&arch=x64&rid=win-x64&os=win10&apphost_version=8.0.1)
+
+***
+
+## Disclaimer of liability:
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+***
+
+## Copyright:
+    --------------------------------------------------------
+    Licensed under the terms of the BSD 3-Clause License
+    (see LICENSE for details).
+    Copyright © 2018-2024, A.A Suvorov
+    All rights reserved.
+    --------------------------------------------------------
